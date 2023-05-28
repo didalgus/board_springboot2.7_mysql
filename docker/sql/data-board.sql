93a92ca17733f83e9229f55f64180e1ace55e2c3
willow <didalgus@gmail.com>
2023-05-28 23:59:58 +0900

    add docker

---
+++ docker/sql/data-board.sql
@@ -0,0 +1,4 @@
+USE demo;
+
+INSERT INTO board (title, content, reg_type, reg_name) VALUES ('title1', 'content1', 'User', 'reg_name1');
+INSERT INTO board (title, content, reg_type, reg_name) VALUES ('title2', 'content2', 'User', 'reg_name2');
---
+++ docker/sql/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS demo;
+CREATE DATABASE demo DEFAULT CHARSET utf8;
+
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER ON `demo`.* TO 'user'@'%';
+
+USE demo;
+
+DROP TABLE if EXISTS board;
+CREATE TABLE board (
+  seq bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '일련번호',
+  title varchar(200) NOT NULL COMMENT '제목',
+  content text NOT NULL COMMENT '내용',
+  reg_type ENUM('User','Kafka') NOT NULL DEFAULT 'User' COMMENT '글 분류 : 사용자(User), Kafka(Kafka)',
+  reg_name varchar(20) NOT NULL COMMENT '등록자이름',
+  reg_dt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT '등록일시',
+  PRIMARY KEY (seq)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='게시판';
